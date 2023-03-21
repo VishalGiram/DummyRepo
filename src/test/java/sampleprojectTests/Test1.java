8ae1ecce284962af48453774406f596ee6192942
PravinJogi <pravin.jogi@eqanim.com>
2023-03-21 12:55:36 +0530

    wait added

--- src/test/java/sampleprojectTests/Test1.java
+++ src/test/java/sampleprojectTests/Test1.java
@@ -1,5 +1,7 @@
 package sampleprojectTests;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -14,5 +16,6 @@ public class Test1 extends base{
 	@Test
 	public void launchURL() {
 		driver.findElement(By.xpath(""));
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 }
